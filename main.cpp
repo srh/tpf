c22ae8f3eb841b95becbc47fb96aae88b50a2796
Sam Hughes <sam@samuelhughes.com>
2026-02-02 14:32:56 -0800

    Simple use of event loop

--- main.cpp
+++ main.cpp
@@ -4,6 +4,11 @@
 
 #include "el/loop.hpp"
 
+void go(el::Loop *loop, std::move_only_function<void ()>&& on_complete) {
+    tpf_setupf("go()...\n");
+    loop->schedule(std::move(on_complete));
+}
+
 int main(int argc, const char **argv) {
     tpf_assert(argc > 0);
     printf("Hello, world!\n");
@@ -11,6 +16,10 @@ int main(int argc, const char **argv) {
     {
         el::Loop loop;
         tpf_setupf("Constructed el::Loop\n");
+
+        loop.schedule([&loop] { go(&loop, [] {
+            tpf_setupf("Finished.\n");
+        }); });
 
         while (loop.has_stuff_to_do()) {
             loop.full_step();
