4487c2f53a9de3f58adb1c8bc6f3420cf42e288e
Sam Hughes <sam@samuelhughes.com>
2026-02-09 13:05:48 -0800

    Spruce up intrusive_list

--- el/intrusive_list.hpp
+++ el/intrusive_list.hpp
@@ -7,11 +7,16 @@ namespace el {
 
 class intrusive_list_node {
     NONCOPYABLE(intrusive_list_node);
+    template <class T>
     friend class intrusive_list;
     // Null for detached list element nodes -- but set to (this, this) for intrusive_list
     // (which is a cyclic doubly linked list).
     intrusive_list_node *prev_ = nullptr;
     intrusive_list_node *next_ = nullptr;
+
+    bool is_properly_detached() const {
+        return prev_ == nullptr && next_ == nullptr;
+    }
 
 protected:
     intrusive_list_node() = default;
@@ -25,8 +30,36 @@ protected:
         }
     }
     void operator=(intrusive_list_node&) = delete;
+
+    void insert_before(intrusive_list_node *node) {
+        tpf_assert(is_properly_detached());
+        intrusive_list_node *prev = node->prev_;
+        tpf_assert(prev->next_ == node);
+        prev->next_ = this;
+        node->prev_ = this;
+        prev_ = prev;
+        next_ = node;
+    }
+
+
+public:
+    intrusive_list_node *next() {
+        return next_;
+    }
+    void detach_self() {
+        prev_->next_ = next_;
+        next_->prev_ = prev_;
+        prev_ = nullptr;
+        next_ = nullptr;
+    }
+
+    bool is_detached() const {
+        return prev_ == nullptr;
+    }
 };
 
+// T* is the element type, T being a subclass of intrusive_list_node.
+template <class T>
 class intrusive_list : private intrusive_list_node {
 public:
     NONCOPYABLE(intrusive_list);
@@ -48,8 +81,13 @@ public:
     intrusive_list_node *self() {
         return this;
     }
-    static intrusive_list_node *next(intrusive_list_node *node) {
-        return node->next_;
+    bool empty() const {
+        return prev_ == this;
+    }
+    void push(T *elem) {
+        intrusive_list_node *node = elem;
+        tpf_assert(node->is_detached());
+        node->insert_before(this);
     }
 };
 
